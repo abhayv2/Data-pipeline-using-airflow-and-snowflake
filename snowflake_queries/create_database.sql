2b5864943728f6aae61b41edbb281ae5c63284f1
abhayv2 <abhayvaidian@gmail.com>
2024-02-24 16:20:15 -0600

    Remove files

--- snowflake_queries/create_database.sql
+++
@@ -1,1 +0,0 @@
-create database IF NOT EXISTS {{params.database_name}}
--- snowflake_queries/create_stage.sql
+++
@@ -1,3 +0,0 @@
-create stage IF NOT EXISTS {{params.stage_name}}
-  STORAGE_INTEGRATION = {{params.storage_int_name}}
-  URL = '{{params.destination_bucket_path}}'
